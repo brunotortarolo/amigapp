796a035a1fd053ca95929a178d78a421d9a13630
Bruno Tortarolo <brunotortarolo@gmail.com>
2022-02-15 17:52:15 -0300

    iu

--- src/app/services/memory-lane.service.ts
+++ src/app/services/memory-lane.service.ts
@@ -4,37 +4,37 @@ import { Memory } from '../models/memory';
 const MEMORIES: Memory[] = [
   {
     id: 1,
-    src: 'https://www.youtube.com/embed/o1dKhSgM3sk',
+    src: 'http://www.youtube.com/embed/o1dKhSgM3sk',
     release_date: '01/01/2019',
     location: ['Valizas', 'Chuy'],
   },
   {
     id: 2,
-    src: 'https://www.youtube.com/embed/Bid_XHCw8Vo',
+    src: 'http://www.youtube.com/embed/Bid_XHCw8Vo',
     release_date: '01/01/2018',
     location: ['Valizas'],
   },
   {
     id: 3,
-    src: 'https://www.youtube.com/embed/boxt-FUU4gY',
+    src: 'http://www.youtube.com/embed/boxt-FUU4gY',
     release_date: '01/01/2017',
     location: ['Valizas', 'Cabo Polonio'],
   },
   {
     id: 4,
-    src: 'https://www.youtube.com/embed/JnjIkqxnP7A',
+    src: 'http://www.youtube.com/embed/JnjIkqxnP7A',
     release_date: '01/01/2016',
     location: ['Valizas'],
   },
   {
     id: 5,
-    src: 'https://www.youtube.com/embed/e2J5w1DTFlI',
+    src: 'http://www.youtube.com/embed/e2J5w1DTFlI',
     release_date: '01/01/2015',
     location: ['Valizas', 'Cabo Polonio'],
   },
   {
     id: 6,
-    src: 'https://www.youtube.com/embed/uRKWSC4CEAU',
+    src: 'http://www.youtube.com/embed/uRKWSC4CEAU',
     release_date: '01/01/2014',
     location: ['La Pedrera', 'Valizas', 'Cabo Polonio'],
   },
